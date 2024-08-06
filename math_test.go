205ab9e01f6b2027d5b17c0388a3c3d560b3ab8a
Vyctor <dev.vyctor@gmail.com>
2024-08-06 19:07:51 -0300

    chore: modificando versao do go

--- math_test.go
+++ math_test.go
@@ -5,6 +5,6 @@ import "testing"
 func TestSoma(t *testing.T) {
 	result := Soma(200, 40)
 	if result != 240 {
-		t.Errorf("Soma(20, 20) = %d; want 240", result)
+		t.Errorf("Soma(200, 40) = %d; want 240", result)
 	}
 }
