d0702e878a1f02856e95ff62808bf21d63189ffe
Vyctor <dev.vyctor@gmail.com>
2024-08-06 18:52:38 -0300

    test: modificando teste

--- math_test.go
+++ math_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestSoma(t *testing.T) {
-	result := Soma(200, 20)
-	if result != 220 {
-		t.Errorf("Soma(20, 20) = %d; want 40", result)
+	result := Soma(200, 40)
+	if result != 240 {
+		t.Errorf("Soma(20, 20) = %d; want 240", result)
 	}
 }
